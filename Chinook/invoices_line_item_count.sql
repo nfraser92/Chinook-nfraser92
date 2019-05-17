68260355e86cc9cf112bf4f076ccc2d6d16e2d26
Niall Fraser <niallfraser92@gmail.com>
2019-05-17 17:11:12 -0500

    more exercises completed

--- Chinook/invoices_line_item_count.sql
+++ Chinook/invoices_line_item_count.sql
@@ -1,1 +1,6 @@
-﻿
+﻿-- Provide a query that shows all Invoices but includes the # of invoice line items.
+SELECT COUNT(il.InvoiceLineId) ItemsOnInvoice, i.InvoiceId
+FROM InvoiceLine il
+INNER JOIN Invoice i
+ON il.InvoiceId = i.InvoiceId
+GROUP By i.InvoiceId;
--- Chinook/sales_agent_total_sales.sql
+++ Chinook/sales_agent_total_sales.sql
@@ -1,1 +1,6 @@
-﻿
+﻿-- Provide a query that shows total sales made by each sales agent.
+SELECT SUM(i.Total) TotalSales, e.EmployeeId, e.FirstName, e.LastName AS EmployeeName
+FROM Invoice i
+JOIN Customer c ON i.CustomerId = c.CustomerId
+JOIN Employee e ON c.SupportRepId = e.EmployeeId
+GROUP BY e.EmployeeId, e.FirstName, e.LastName;
